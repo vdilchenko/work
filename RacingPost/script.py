010275fd509ae696773042ae9a71be33049b80bd
vdilchenko <43196454+vdilchenko@users.noreply.github.com>
2018-09-15 17:49:27 +0600

    Update script.py

--- RacingPost/script.py
+++ RacingPost/script.py
@@ -23,9 +23,10 @@ def login(url, email, password):
     :param email: email for the login
     :param password: password for the login
     """
-
+    count = 0
     while True:
         try:
+            count += 1
             driver.get(url)
             driver.find_element_by_id('CybotCookiebotDialogBodyButtonAccept').click()
             driver.get(url)
@@ -33,7 +34,7 @@ def login(url, email, password):
                                           '//div//div//div//div')[0].click()
             break
         except WebDriverException:
-            print("Reload %s" %counter)
+            print("Reload %s" % count)
 
     driver.find_elements_by_xpath('//div[@class="fields__validationWrapper___3D3Kv"]')[0]\
         .find_element_by_tag_name('input').send_keys(email)
@@ -84,7 +85,7 @@ def count_pages(soup):
 
 
 if __name__ == '__main__':
-    chromedriver = '/Users/ilchenkoslava/Downloads/chromedriver3'
+    chromedriver = 'chromedriver.exe'
     driver = webdriver.Chrome(chromedriver)
 
     login('https://www.racingpost.com/', 'jmcatelen@hotmail.com', '22Muyse22')
@@ -99,11 +100,13 @@ if __name__ == '__main__':
     while True:
         try:
             for k in keywords[keywords.index('aas')+1:]:
+                # for key, value in lists.items():
+                #     print(key, len(value))
                 copy_list = lists
                 if counter > 100:
                     print(k)
-                    counter = 0
                     save_to_excel(lists, counter)
+                    counter = 0
                     lists = collections.defaultdict(list)
 
                 driver.get(query_url % (1, k))
@@ -134,7 +137,7 @@ if __name__ == '__main__':
                             female = []
                             try:
                                 table = driver.find_element_by_class_name('pedigreetable')
-                                abc = 'ch.', 'gr.', 'b.', 'br.', 'blk.', 'dkb/br.', 'blk/br.', '1955'
+                                abc = 'ch.', 'gr.', 'b.', 'br.', 'blk.', 'dkb/br.', 'blk/br.', 'chuck.'
                                 if table.find_elements_by_class_name('w'):
                                     print('a')
                                     for zz in range(1, 63):
@@ -142,20 +145,26 @@ if __name__ == '__main__':
                                     continue
 
                                 for m in table.find_elements_by_class_name('m'):
-                                    if m.text.startswith(abc):
+                                    horsename_m = m.find_element_by_xpath('//a[@class="horseName"]').text
+                                    if (horsename_m.startswith(abc)) or (horsename_m.isupper() is False):
                                         continue
-                                    male.append((m.get_attribute('data-g'), m.text.replace('\n', '')))
+                                    male.append((m.get_attribute('data-g'), horsename_m.replace('\n', '')))
 
                                 for f in table.find_elements_by_class_name('f'):
-                                    if f.text.startswith(abc):
+                                    horsename_f = f.find_element_by_xpath('//a[@class="horseName"]').text
+                                    if (horsename_f.startswith(abc)) or (horsename_f.isupper() is False):
                                         continue
-                                    female.append((f.get_attribute('data-g'), f.text.replace('\n', '')))
+                                    female.append((f.get_attribute('data-g'), horsename_f.replace('\n', '')))
+                                print(male)
+                                print(female)
 
                                 for i, ma in enumerate(sorted(male, key=lambda i: i[0]), 1):
                                     lists[str(i)].append(ma[1])
                                 for ii, fe in enumerate(sorted(female, key=lambda i: i[0]), 1):
-                                    lists[str(len(male) + ii)].append(fe[1])
-                            except (StaleElementReferenceException, NoSuchElementException):
+                                        lists[str(len(male) + ii)].append(fe[1])
+                            except (StaleElementReferenceException, NoSuchElementException) as e:
+                                print(e)
+                                print('raz')
                                 for zz in range(1, 63):
                                     lists[str(zz)].append('-')
                             except TimeoutException:
@@ -163,6 +172,21 @@ if __name__ == '__main__':
 
                         for horse_url in horses_urls:
                             driver.get(horse_url)
+                            try:
+                                if driver.find_element_by_class_name('p404-error'):
+                                    print('No data')
+                                    for key, v in lists.items():
+                                        try:
+                                            if isinstance(int(key), int):
+                                                continue
+                                        except ValueError:
+                                            if key == 'Fullname':
+                                                continue
+                                            v.append('-')
+                                    continue
+                            except NoSuchElementException:
+                                pass
+
                             time.sleep(3)
                             counter += 1
                             print(counter)
@@ -204,3 +228,4 @@ if __name__ == '__main__':
             print(k)
             save_to_excel(copy_list, counter)
     save_to_excel(lists, counter)
+    
