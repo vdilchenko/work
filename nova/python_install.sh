9dce90ec8413f2d3affc284a5850b6679176b1ca
Vyacheslav <vdilchenko@gmail.com>
2020-11-01 12:32:40 +0600

    fixed typos

--- nova/python_install.sh
+++ nova/python_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Installing Python3.7" 
+echo "Installing Python3.8" 
 sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.7
-python3.7 --version 
-echo "python3.7 is installed"
+sudo apt install python3
+python3 --version 
+echo "python3 is installed"
 echo "---------------------"
 echo "---------------------"
 echo "---------------------"
@@ -15,5 +15,4 @@ sudo apt update
 sudo apt install python3-pip
 pip3 --version 
 echo "pip3 installed"
-sudo apt install chromium-chromedriver
-echo "chromedriver installed"
+
